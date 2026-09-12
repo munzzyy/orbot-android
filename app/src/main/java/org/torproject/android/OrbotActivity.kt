060ccab4154be04b02cb5553f2b9e40920b1aa54
bitmold <dsnake@protonmail.com>
2026-09-12 19:45:22 -0400

    Additional small cleanups re #1812 - use constant for the -1 when HTTP/SOCKS/etc ports aren't set, instead of a lot of magic nums in OrbotActivity/MoreFragment

--- app/src/main/java/org/torproject/android/OrbotActivity.kt
+++ app/src/main/java/org/torproject/android/OrbotActivity.kt
@@ -44,8 +44,8 @@ class OrbotActivity : BaseActivity() {
 
     private var lastNavMenuIndex = -1
 
-    var portSocks: Int = -1
-    var portHttp: Int = -1
+    var portSocks: Int = PORT_NOT_SET_VALUE
+    var portHttp: Int = PORT_NOT_SET_VALUE
 
     // used to hide UI while password isn't obtained
     private var rootLayout: View? = null
@@ -70,8 +70,8 @@ class OrbotActivity : BaseActivity() {
         // programmatically set title to "Orbot" since camo mode will overwrite it here from manifest
         title = getString(R.string.app_name)
         savedInstanceState?.let {
-            portSocks = it.getInt(BUNDLE_KEY_SOCKS, -1)
-            portHttp = it.getInt(BUNDLE_KEY_HTTP, -1)
+            portSocks = it.getInt(BUNDLE_KEY_SOCKS, PORT_NOT_SET_VALUE)
+            portHttp = it.getInt(BUNDLE_KEY_HTTP, PORT_NOT_SET_VALUE)
         }
         try {
             createOrbot()
@@ -347,5 +347,8 @@ class OrbotActivity : BaseActivity() {
 
         // Make sure this is only shown once per app-start, not on every device rotation.
         private var rootDetectionShown = false
+
+        // default values for HTTP/SOCKS ports, see #1812
+        const val PORT_NOT_SET_VALUE = -1
     }
 }
--- app/src/main/java/org/torproject/android/ui/more/MoreFragment.kt
+++ app/src/main/java/org/torproject/android/ui/more/MoreFragment.kt
@@ -24,8 +24,8 @@ import org.torproject.android.util.sendIntentToService
 import org.torproject.jni.TorService
 
 class MoreFragment : Fragment() {
-    private var httpPort = -1
-    private var socksPort = -1
+    private var httpPort = OrbotActivity.PORT_NOT_SET_VALUE
+    private var socksPort = OrbotActivity.PORT_NOT_SET_VALUE
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
