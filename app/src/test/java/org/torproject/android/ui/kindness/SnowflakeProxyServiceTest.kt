a1d0c1f595d76b6bd5d7e5ae51ed43a319e4c62d
bitmold <dsnake@protonmail.com>
2026-09-12 20:51:38 -0400

    add comments to SnowflakeProxyServiceTest for tests pertaining to #1808 and #1810

--- app/src/test/java/org/torproject/android/ui/kindness/SnowflakeProxyServiceTest.kt
+++ app/src/test/java/org/torproject/android/ui/kindness/SnowflakeProxyServiceTest.kt
@@ -6,12 +6,17 @@ import org.junit.Test
 import org.torproject.android.util.Prefs
 
 class SnowflakeProxyServiceTest {
+
+    // see: https://github.com/guardianproject/orbot-android/pull/1808
+    // see: https://github.com/guardianproject/orbot-android/pull/1810
     @Test
     fun relevantPreferenceChangesAreHandled() {
         assertFalse(shouldIgnoreSnowflakePreferenceChange(Prefs.PREF_BRIDGE_COUNTRY))
         assertFalse(shouldIgnoreSnowflakePreferenceChange(Prefs.PREF_CAMO_APP_PACKAGE))
     }
 
+    // see: https://github.com/guardianproject/orbot-android/pull/1808
+    // see: https://github.com/guardianproject/orbot-android/pull/1810
     @Test
     fun unrelatedPreferenceChangesAreIgnored() {
         assertTrue(shouldIgnoreSnowflakePreferenceChange("unrelated_preference"))
